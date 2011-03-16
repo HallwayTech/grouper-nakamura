c5b97a45339f2e451ef06ba9efe3b7f27e96518f
Erik Froese <erik.froese@gmail.com>
2011-03-16 16:19:47 -0400

    Make the component configurable (almost).

--- event-handler/src/main/java/edu/nyu/grouper/osgi/GrouperEventHandler.java
+++ event-handler/src/main/java/edu/nyu/grouper/osgi/GrouperEventHandler.java
@@ -1,17 +1,23 @@
 package edu.nyu.grouper.osgi;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Dictionary;
+
 import org.apache.felix.scr.annotations.Component;
-import org.apache.felix.scr.annotations.Service;
 import org.apache.felix.scr.annotations.Properties;
 import org.apache.felix.scr.annotations.Property;
+import org.apache.felix.scr.annotations.Service;
+import org.osgi.service.cm.ConfigurationException;
+import org.osgi.service.cm.ManagedService;
 import org.osgi.service.event.Event;
-import org.osgi.service.event.EventHandler;
 import org.osgi.service.event.EventConstants;
+import org.osgi.service.event.EventHandler;
 import org.sakaiproject.nakamura.api.lite.util.Iterables;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-@Service
+@Service(value = EventHandler.class)
 @Component(immediate = true)
 @Properties(value = { 
 	@Property(name = EventConstants.EVENT_TOPIC, 
@@ -19,16 +25,99 @@ import org.slf4j.LoggerFactory;
 			"org/sakaiproject/nakamura/lite/authorizables/ADDED",
 			"org/sakaiproject/nakamura/lite/authorizables/DELETE"})
 })
-public class GrouperEventHandler implements EventHandler {
+public class GrouperEventHandler implements EventHandler, ManagedService {
 
 	private static final Logger log = LoggerFactory.getLogger(GrouperEventHandler.class);
-	
+
+	// Configurable via the ConfigAdmin services.
+	private static final String DEFAULT_URL = "http://localhost:8080/grouper-ws";
+	@Property(value=DEFAULT_URL)
+	private static final String PROP_URL = "sakai.grouper.url";
+
+	private static final String DEFAULT_USERNAME = "GrouperSystem";
+	@Property(value=DEFAULT_USERNAME)
+	private static final String PROP_USERNAME = "sakai.grouper.username";
+
+	private static final String DEFAULT_PASSWORD = "abc123";
+	@Property(value=DEFAULT_PASSWORD)
+	private static final String PROP_PASSWORD = "sakai.grouper.password";
+
+	private static final String DEFAULT_BASESTEM = "edu:apps:sakai3";
+	@Property(value=DEFAULT_BASESTEM)
+	private static final String PROP_BASESTEM = "sakai.grouper.basestem";
+
+	private URL url;
+	private String username;
+	private String password;
+	private String baseStem;
+
 	public void handleEvent(Event event) {
 		StringBuffer buffer = new StringBuffer();
 		for(String name: Iterables.of(event.getPropertyNames())){
 			buffer.append("\n" + name + "=" + event.getProperty(name));
 		}
 		log.info("topic : " + event.getTopic() + " properties: " + buffer.toString());
+
+		if ("org/sakaiproject/nakamura/lite/authorizables/ADDED".equals(event.getTopic())){
+
+		}
+		if ("org/sakaiproject/nakamura/lite/authorizables/DELETE".equals(event.getTopic())){
+
+		}
 	}
 
+	// -------------------------- Configuration Admin --------------------------
+
+	/**
+	 * Called by the Configuration Admin service when a new configuration is 
+	 * detected by Fileinstall
+	 */
+	@SuppressWarnings("rawtypes")
+	public void updated(Dictionary props) throws ConfigurationException {
+		try {
+			setUrl((String)props.get(PROP_URL));
+			setUsername((String)props.get(PROP_USERNAME));
+			setPassword((String)props.get(PROP_PASSWORD));
+			setBaseStem((String)props.get(PROP_BASESTEM));
+		}
+		catch (MalformedURLException mfe) {
+			throw new ConfigurationException(PROP_URL, mfe.getMessage(), mfe);
+		}
+	}
+
+	public URL getUrl() {
+		return url;
+	}
+
+	public void setUrl(URL url) {
+		this.url = url;
+	}
+
+	public void setUrl(String urlString) throws MalformedURLException {
+		setUrl(new URL(urlString));
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getBaseStem() {
+		return baseStem;
+	}
+
+	public void setBaseStem(String baseStem) {
+		this.baseStem = baseStem;
+	}
 }
