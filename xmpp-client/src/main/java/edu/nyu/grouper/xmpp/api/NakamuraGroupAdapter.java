edc0bbfe29fc80d19ed0d6cc524dfd2d1758934d
Erik Froese <erik.froese@gmail.com>
2011-03-24 10:19:46 -0400

    Clarify for a group in Grouper: groupName ~= 98765364893576894572345978 groupId   ~= edu:apps:sakai:dept:groupName Can't assume the group is still there when we delete it.

--- xmpp-client/src/main/java/edu/nyu/grouper/xmpp/api/NakamuraGroupAdapter.java
+++ xmpp-client/src/main/java/edu/nyu/grouper/xmpp/api/NakamuraGroupAdapter.java
@@ -7,31 +7,35 @@ public interface NakamuraGroupAdapter {
 	
 	/**
 	 * Create a group.
-	 * @param groupId
+	 * @param groupId the internal grouper id
+	 * @param groupName the full name of the group (includes stem)
 	 * @throws GroupModificationException
 	 */
 	public void createGroup(Group group) throws GroupModificationException;
 	
 	/**
 	 * Delete a group.
-	 * @param groupId
+	 * @param groupId the internal grouper id
+	 * @param groupName the full name of the group (includes stem)
 	 * @throws GroupModificationException
 	 */
-	public void deleteGroup(Group group) throws GroupModificationException;
+	public void deleteGroup(String groupId, String groupName) throws GroupModificationException;
 	
 	/**
 	 * Add a subject to a group.
-	 * @param groupId
-	 * @param subject
+	 * @param groupId the internal grouper id
+	 * @param groupName the full name of the group (includes stem)
+	 * @param subjectId the id of the subject being added
 	 * @throws GroupModificationException
 	 */
-	public void addMembership(String groupId, String groupExtension, String subjectId) throws GroupModificationException;
+	public void addMembership(String groupId, String groupName, String subjectId) throws GroupModificationException;
 	
 	/**
 	 * Remove a subject from a group.
-	 * @param groupId
-	 * @param subject
+	 * @param groupId the internal grouper id
+	 * @param groupName the full name of the group (includes stem)
+	 * @param subjectId the id of the subject being removed
 	 * @throws GroupModificationException
 	 */
-	public void deleteMembership(String groupId, String groupExtension, String subjectId) throws GroupModificationException;
+	public void deleteMembership(String groupId, String groupName, String subjectId) throws GroupModificationException;
 }
