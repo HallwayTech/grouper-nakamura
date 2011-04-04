89aa2351174b3011004e43e8eb83a8a7528bb2a7
Erik Froese <erik@nyu.edu>
2011-04-04 09:19:42 -0400

    Simplify the API. Rename the base impl.

--- event-handler/src/main/java/edu/nyu/grouper/api/GrouperIdHelper.java
+++ event-handler/src/main/java/edu/nyu/grouper/api/GrouperIdHelper.java
@@ -6,10 +6,6 @@ package edu.nyu.grouper.api;
  * 
  * groupId is the authorizableId if of the form some_thing_name1
  * baseStem is of the form edu:apps:sakai3
- * 
- * The full name will be edu:apps:sakai3:some:thing:name1
- * The full stem will be edu:apps:sakai3:some:thing
- * The name will be name1
  */
 public interface GrouperIdHelper {
 
@@ -18,19 +14,11 @@ public interface GrouperIdHelper {
 	 * @param the authorizableId of the Group
 	 * @return the fully qualified name of this group in Grouper
 	 */
-	public String getFullGrouperName(String baseStem, String groupId);
+	public String getGrouperName(String groupId);
 	
 	/**
-	 * @param baseStem the folder in Grouper for sakai3
-	 * @param the authorizableId of the Group
-	 * @return the full stem representing the folder this group is in Grouper
+	 * @param groupId
+	 * @return the Grouper group extension
 	 */
-	public String getFullStem(String baseStem, String groupId);
-	
-	/**
-	 * @param baseStem the folder in Grouper for sakai3
-	 * @param the authorizableId of the Group
-	 * @return the full stem representing the Id of this group in Grouper
-	 */
-	public String getName(String groupId);
+	public String getGrouperExtension(String groupId);
 }
