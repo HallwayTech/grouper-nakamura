ee032d1d181c7d52e1478d3c731d4964bb177ca2
Erik Froese <erik.froese@gmail.com>
2011-04-04 14:56:48 -0400

    Add a helper to translate sakai names into grouper names using an aggregate group strategy.

---
+++ event-handler/src/main/java/edu/nyu/grouper/AggregateGroupsGrouperIdHelperImpl.java
@@ -0,0 +1,104 @@
+package edu.nyu.grouper;
+
+import org.apache.felix.scr.annotations.Component;
+import org.apache.felix.scr.annotations.Reference;
+import org.apache.felix.scr.annotations.Service;
+import org.sakaiproject.nakamura.api.lite.authorizable.Group;
+
+import edu.nyu.grouper.api.GrouperIdHelper;
+import edu.nyu.grouper.osgi.api.GrouperConfiguration;
+
+/**
+ * 
+ * Map SakaiOAE groupIds to Grouper group names.
+ * 
+ * In SakaiOAE each {@link Group} has its own members and a managers group.
+ * In Grouper these are located at:
+ * some:base:stem:grouperId:members = UNION(inst:grouperId:all, sakai:grouperId:members_sakaioae)
+ * some:base:stem:grouperId:managers = UNION(inst:grouperId:instructors, sakai:grouperId:managers_sakaioae)
+ * 
+ * Each of those two groups contains the union of the membership of two or more subgroups.
+ * One or more those groups is managed by the institution. They should reside in a 
+ * different stem where SakaiOAE doesn't have write access.
+ * 
+ * The other subgroup of the members and manager groups is where Sakai attempts
+ * to reads and write.
+ * some:base:stem:grouperId:members_sakaioae
+ * some:base:stem:grouperId:managers_sakaioae
+ *
+ */
+@Service
+@Component
+public class AggregateGroupsGrouperIdHelperImpl implements GrouperIdHelper {
+	
+	@Reference
+	protected GrouperConfiguration grouperConfiguration;
+
+	public String getGrouperName(String groupId) {
+		if (groupId == null){
+			return null;
+		}
+		return getFullStem(groupId) + ":" + getGrouperExtension(groupId);
+	}
+
+	/**
+	 * Get the extension for this group in Grouper.
+	 * The extension is the last component in a Grouper name
+	 */
+	public String getGrouperExtension(String groupId) {
+		if (groupId == null){
+			return null;
+		}
+		String extension;
+		if(isManagersGroup(groupId)){
+			extension = "managers_" + grouperConfiguration.getSuffix();
+		}
+		else {
+			extension = "members_" + grouperConfiguration.getSuffix();
+		}
+		return extension;
+	}
+	
+	/**
+	 * @return the stem for this SakaiOAE group in Grouper.
+	 * 
+	 * groupId == some_thing => base:stem:some:thing
+	 * groupId == some_thing-managers => base:stem:some:thing
+	 */
+	public String getFullStem(String groupId){
+		if (groupId == null){
+			return null;
+		}
+		int indexOfManagers = groupId.indexOf("-managers");
+		if (indexOfManagers != -1){
+			groupId = groupId.substring(0, indexOfManagers);
+		}
+		return grouperConfiguration.getBaseStem() + ":" + getPartialStem(groupId);
+	}
+	
+	/**
+	 * Convert the groupId into a stem.
+	 * this_group_name => this:group:name
+	 * 
+	 * @param groupId the sakaiOAE groupId
+	 * @return a partial stem for the corresponding grouper Group
+	 */
+	private String getPartialStem(String groupId){
+		if (groupId == null){
+			return null;
+		}
+		return groupId.replaceAll("_", ":");
+	}
+	
+	/**
+	 * @param groupId
+	 * @return whether or not this is a managers group for another group
+	 */
+	private boolean isManagersGroup(String groupId){
+		return groupId.endsWith("-managers");
+	}
+	
+	public void bindGrouperConfiguration(GrouperConfiguration config){
+		this.grouperConfiguration = config;
+	}
+}
---
+++ event-handler/src/test/java/edu/nyu/grouper/osgi/AggregateGroupsGrouperIdHelperImplTest.java
@@ -0,0 +1,70 @@
+package edu.nyu.grouper.osgi;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+import junit.framework.TestCase;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import edu.nyu.grouper.AggregateGroupsGrouperIdHelperImpl;
+import edu.nyu.grouper.osgi.api.GrouperConfiguration;
+
+public class AggregateGroupsGrouperIdHelperImplTest extends TestCase {
+
+	private AggregateGroupsGrouperIdHelperImpl idHelper;
+	private static final String BASE_STEM = "some:base:stem";
+	
+	@Before
+	public void setUp() throws Exception {
+		// Test grouper configs
+		Map<String,String> m = new HashMap<String,String>();
+		m.put(GrouperConfigurationImpl.PROP_BASESTEM, BASE_STEM);
+		GrouperConfigurationImpl gconfig = new GrouperConfigurationImpl();
+		gconfig.updated(m);
+		
+		idHelper = new AggregateGroupsGrouperIdHelperImpl();
+		idHelper.bindGrouperConfiguration((GrouperConfiguration)gconfig);
+	}
+	
+	@Test
+	public void testGetGrouperName(){
+		assertEquals(null, idHelper.getGrouperName(null));
+		assertEquals(BASE_STEM + ":group1:members_sakaioae",
+					idHelper.getGrouperName("group1"));
+		assertEquals(BASE_STEM + ":group1:managers_sakaioae",
+					idHelper.getGrouperName("group1-managers"));
+		
+		assertEquals(BASE_STEM + ":gstem1:gstem2:gext:members_sakaioae",
+					idHelper.getGrouperName("gstem1_gstem2_gext"));
+		assertEquals(BASE_STEM + ":gstem1:gstem2:gext:managers_sakaioae",
+					idHelper.getGrouperName("gstem1_gstem2_gext-managers"));
+	}
+	
+	@Test
+	public void testGetGrouperExtension(){
+		assertEquals("members_sakaioae",
+				idHelper.getGrouperExtension("group1"));
+		assertEquals("managers_sakaioae",
+				idHelper.getGrouperExtension("group1-managers"));
+		
+		assertEquals("members_sakaioae",
+				idHelper.getGrouperExtension("group1"));
+		assertEquals("managers_sakaioae",
+				idHelper.getGrouperExtension("group1-managers"));
+	}
+	
+	@Test
+	public void testGetFullStem(){
+		assertEquals(null, idHelper.getFullStem(null));
+		
+		assertEquals(BASE_STEM + ":group1", idHelper.getFullStem("group1"));
+		assertEquals(BASE_STEM + ":group1", idHelper.getFullStem("group1-managers"));
+		
+		assertEquals(BASE_STEM + ":stem1:group1", idHelper.getFullStem("stem1_group1"));
+		assertEquals(BASE_STEM + ":stem1:group1", idHelper.getFullStem("stem1_group1-managers"));
+	}
+	
+}
