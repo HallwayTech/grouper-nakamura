2f835ae74769deb957391cf14d0d967e447f6893
Erik Froese <erik.froese@gmail.com>
2011-04-07 11:30:01 -0400

    Get properties from the parent stem. Separate properties for -managers groups.

--- esb/src/main/java/edu/nyu/grouper/util/StaticInitialGroupPropertiesProvider.java
+++ esb/src/main/java/edu/nyu/grouper/util/StaticInitialGroupPropertiesProvider.java
@@ -6,25 +6,34 @@ import edu.internet2.middleware.grouper.Group;
 import edu.nyu.grouper.util.api.InitialGroupPropertiesProvider;
 
 /**
- * Provide a set of static properties for new sakai groups.
+ * Set HTTP POST params that become properties on the group in SakaiOAE
  * @author froese
  */
 public class StaticInitialGroupPropertiesProvider implements InitialGroupPropertiesProvider {
 
 	public void addProperties(Group group, String nakamuraGroupId, PostMethod method) {
-		
+
 		// Basic info
 		method.addParameter("sakai:group-id", nakamuraGroupId);
-		method.addParameter("sakai:group-title", group.getExtension());
-		method.addParameter("sakai:group-description", group.getDescription());
+		method.addParameter("sakai:group-description", group.getParentStem().getDescription());
 		method.addParameter(":sakai:pages-template", "/var/templates/site/defaultgroup");
-		
+
 		// Authorizations
 		method.addParameter(":sakai:manager", "admin");
-		method.addParameter("sakai:pages-visible", "members-only");
-		method.addParameter("group-joinable", "no");
-		method.addParameter("group-visible", "members-only");
-		
+
+		// Managers groups
+		if (nakamuraGroupId.endsWith("-managers")) {
+			method.addParameter("sakai:bare", "true");
+			method.addParameter("sakai:managed-group", 
+					nakamuraGroupId.substring(0, nakamuraGroupId.indexOf("-managers")));
+		}
+		else {
+			method.addParameter("sakai:group-title", group.getParentStem().getExtension());
+			method.addParameter("sakai:pages-visible", "members-only");
+			method.addParameter("group-joinable", "no");
+			method.addParameter("group-visible", "members-only");
+		}
+
 		// Grouper
 		method.addParameter("grouper:name", group.getName());
 		method.addParameter("grouper:uuid", group.getUuid());
