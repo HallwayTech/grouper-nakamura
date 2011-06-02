8b028096b6e8061a046ce7f156d98cc8a12f0a34
Erik Froese <erik.froese@gmail.com>
2011-06-02 15:35:38 -0400

    Add license and remove SPECIAL_GROUP_SUFFIXES.

--- src/main/java/org/sakaiproject/nakamura/grouper/PropertyGrouperIdProviderImpl.java
+++ src/main/java/org/sakaiproject/nakamura/grouper/PropertyGrouperIdProviderImpl.java
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Sakai Foundation (SF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The SF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
 package org.sakaiproject.nakamura.grouper;
 
 import org.apache.felix.scr.annotations.Reference;
@@ -17,20 +34,18 @@ import org.slf4j.LoggerFactory;
 public class PropertyGrouperIdProviderImpl implements GrouperIdManager {
 
 	private static final Logger log = LoggerFactory.getLogger(PropertyGrouperIdProviderImpl.class);
-	
+
 	@Reference
 	protected GrouperConfiguration grouperConfiguration;
-	
+
 	@Reference
 	protected Repository repository;
-	
-	public static String[] SPECIAL_GROUP_SUFFIXES = new String[] {"-managers", "-ta", "-lecturer"};
 
 	@Override
 	public String getGrouperName(String groupId) {
 		return getProperty(groupId, GrouperManager.GROUPER_NAME_PROP);
 	}
-	
+
 	private String getProperty(String groupId, String propertyName){
 		String propValue = null;
 		Authorizable authorizable = null;
@@ -71,5 +86,4 @@ public class PropertyGrouperIdProviderImpl implements GrouperIdManager {
 		}
 		return propValue;
 	}
-
 }
