b390c39e79bf3586f671a882fbd950df7488cc73
Erik Froese <erik.froese@gmail.com>
2011-07-14 16:03:38 -0400

    Fix the create user script.

--- src/main/bash/create_grouper_admin.sh
+++ src/main/bash/create_grouper_admin.sh
@@ -3,6 +3,6 @@
 ADMIN_PASS="admin"
 HOST="localhost:8080"
 
-curl -w '%{http_code}' -u admin:$ADMIN_PASS -F:name=grouper-admin -F:pwd=grouper -F:pwdConfirm=grouper http://$HOST/system/userManager/group.create.json
+curl -w '%{http_code}' -u admin:$ADMIN_PASS -F:name=grouper-admin -Fpwd=grouper -FpwdConfirm=grouper http://$HOST/system/userManager/user.create.json
 
 curl -w '%{http_code}' -u admin:$ADMIN_PASS -F:member=grouper-admin http://$HOST/system/userManager/group/administrators.update.json
